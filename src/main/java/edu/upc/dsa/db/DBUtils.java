bd5b9937251fccc90d23ec5a803d377985039c53
JanaCorsellas <jana.corsellas@estudiantat.upc.edu>
2024-06-19 04:05:20 +0200

    v11

--- src/main/java/edu/upc/dsa/db/DBUtils.java
+++ src/main/java/edu/upc/dsa/db/DBUtils.java
@@ -8,7 +8,7 @@ public class DBUtils {
 
     public static final String DB_NAME = "scaperoom";
     public static final String DB_HOST = "127.0.0.1";
-    public static final String DB_USER = "root";
+    public static final String DB_USER = "dsa2";
     public static final String DB_PASS = "Mazinger72";
     public static final String DB_PORT = "3306";
 
